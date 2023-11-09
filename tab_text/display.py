acf1ee83c83412160a92ed54e31e4cee0ed29b59
phuonganh38 <144893927+phuonganh38@users.noreply.github.com>
2023-11-09 16:25:58 +1100

    Update display.py

--- tab_text/display.py
+++ tab_text/display.py
@@ -27,4 +27,27 @@ def display_tab_text_content(file_path=None, df=None):
     -> None
 
     """
+    # Instantiate TextColumn class
+    text_column = TextColumn(file_path=file_path, df=df)
+
+    # Save TextColumn class into Streamlit session state
+    st.session_state.text_column = text_column
+
+    # Find text columns
+    st.session_state.text_column.find_text_cols()
+
+    # Display select box with text columns
+    st.session_state.selected_text_col = st.selectbox("Select a text column", st.session_state.text_column.text_cols)
+
+    # If a text column is selected, compute information to be displayed
+    if st.session_state.selected_text_col is not None:
+        st.session_state.text_column.set_data(selected_text_col=st.session_state.selected_text_col)
+
+        # Display results in an expander container
+        with st.expander("ℹ️ - Text Column Summary", expanded=True):
+            st.write(st.session_state.text_column.get_summary())
+            st.altair_chart(st.session_state.text_column.histogram)
+            st.write(st.session_state.text_column.frequent)
+
+
     
